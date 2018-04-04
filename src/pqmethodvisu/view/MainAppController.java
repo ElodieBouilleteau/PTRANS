102d67f89027f6d628d16d926c82189dfdf5e148
Elodie BOUILLETEAU <elodie.bouilleteau@etu.univ-nantes.fr>
2018-04-04 14:06:50 +0200

    Modification + erreur chemin controller dans MainApp.fxml

--- src/pqmethodvisu/view/MainAppController.java
+++ src/pqmethodvisu/view/MainAppController.java
@@ -1,27 +1,42 @@
 package pqmethodvisu.view;
 
-import java.awt.event.ActionListener;
-
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import pqmethodvisu.FXUtils;
 
-
-public class MainLayoutController implements ActionListener  {
+public class MainAppController {
 	
+	/*
+	@FXML
+	private AnchorPane ap;
 	private String value;
+	
+	@FXML
+	private void addItemsOnCb()
+	{
+		System.out.println("test");
+		ComboBox cb = FXUtils.getChildByID(ap, "facteurPrincipalCombobox");
+        ObservableList<String> list = FXCollections.observableArrayList("1","2","3","4");
+        cb.setItems(list);
+	}
+	
 	
 	@FXML
 	private void affichePopup()
 	{
-		//System.out.println("test");
+		System.out.println("test");
 		final Stage dialog = new Stage();
         dialog.initModality(Modality.APPLICATION_MODAL);
         VBox dialogVbox = new VBox(20);
@@ -40,11 +55,5 @@ public class MainLayoutController implements ActionListener  {
         	}
         });
 	}
-
-	@Override
-	public void actionPerformed(java.awt.event.ActionEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
+	*/
 }
